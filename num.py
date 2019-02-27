218fc05fa57413438a4a35cfece9638499689c0d
JJEAN <jennyjean423@gmail.com>
2019-02-27 11:06:37 +0800

    update times

--- num.py
+++ num.py
@@ -1,12 +1,15 @@
 import random
 r = random.randint(1, 100)
+count = 0
 while True:
-	num = input('猜一個數字')
-	num = int(num)
-	if num == r:
-	    print('猜對！！')
-	    break
-	elif num > r:
-	    print('再試試看，答案比你猜的小')
-	elif num < r:
-	    print('再試試看，答案比你猜的大')
+    count += 1
+    num = input('猜一個數字')
+    num = int(num)
+    if num == r:
+        print('猜對！！')
+        break
+    elif num > r:
+        print('再試試看，答案比你猜的小')
+    elif num < r:
+        print('再試試看，答案比你猜的大')
+    print('這是你猜的第', count, '次')
